ccc0fc869fe086c6a3e7d60ee13af6ed68b0f211
manoj-pillalamarri <manojpillalamari.5@gmail.com>
2025-07-21 12:12:19 +0530

    First commit to smartformatter repo

---
+++ .ipynb_checkpoints/setup-checkpoint.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='smartformatter',
+    version='0.1',
+    pacakges=find_packages(),
+    install_requies=['inflect'],
+    author='Manoj',
+    description='Utility functions for smart formatting of names, phones, numbers.',
+)
